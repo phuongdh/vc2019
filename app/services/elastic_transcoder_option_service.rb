64f548d6fbed0ff5a4d398710afdd6033f24ee8a
Phuong Dinh <phuongdh@gmail.com>
2019-04-18 15:31:49 -0400

    Init

---
+++ app/services/elastic_transcoder_option_service.rb
@@ -0,0 +1,16 @@
+class ElasticTranscoderOptionService
+  # Returns default audio/video output options for ActiveEncode.
+  # @param fileset the data to be persisted
+  # @return an array of hashes of output options for audio/video
+  def self.call(file_set)
+    file_name = file_set.label
+    options = { pipeline_id: "1548794014119-h8cf0r", masterfile_bucket: "active-encode-test0" }
+
+    options[:outputs] = [{ key: "quality-medium/hls/#{file_name}",
+                           preset_id: "1351620000001-000010" },
+                         { key: "quality-high/hls/#{file_name}",
+                           preset_id: "1351620000001-000001" }]
+    [options]
+  end
+
+end
